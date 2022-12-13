9b2e2842d148322a1cc84d6cd64143287530aa08
sepehr.sadri <sepehr.sadri@balad.ir>
2022-12-14 02:36:24 +0330

    feature: init project

---
+++ app/src/main/java/com/sadri/kitten/ui/theme/Theme.kt
@@ -0,0 +1,46 @@
+package com.sadri.kitten.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+  primary = DarkGreys10,
+  primaryVariant = DarkGreys40,
+  secondary = LightKitten,
+  background = Color.Transparent,
+  surface = DarkGreys20,
+  onPrimary = DarkGreys90,
+  onBackground = DarkGreys70,
+  onSurface = DarkGreys90
+)
+
+private val LightColorPalette = lightColors(
+  primary = LightGreys10,
+  primaryVariant = LightGreys40,
+  secondary = LightKitten,
+  background = Color.Transparent,
+  surface = LightGreys20,
+  onPrimary = LightGreys90,
+  onBackground = LightGreys70,
+  onSurface = LightGreys90
+)
+
+@Composable
+fun KittenTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+  val colors = if (darkTheme) {
+    DarkColorPalette
+  } else {
+    LightColorPalette
+  }
+
+  MaterialTheme(
+    colors = colors,
+    typography = Typography,
+    shapes = Shapes,
+    content = content
+  )
+}
